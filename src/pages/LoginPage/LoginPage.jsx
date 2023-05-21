4f160637fea2156f06b5edec014e3381a27d53ee
mirkek <asen.mirkhat@bk.ru>
2023-05-21 14:57:31 +0600

    login animate

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,12 @@
 import {NavLink} from "react-router-dom";
 import {useState} from "react";
 import {motion as m} from "framer-motion";
+import Lottie from "lottie-react";
 
 import Logo from "../../assets/images/logo_img.png";
 import AuthInput from "../../components/form/Auth/AuthInput/AuthInput";
 import AuthButton from "../../components/form/Auth/AuthButton/AuthButton";
+import CheckAnimation from "../../assets/gifs/check.json";
 
 import {login} from "../../api/UserAPI.jsx";
 
@@ -16,6 +18,7 @@ const LoginPage = () => {
 
     const [formData, setData] = useState(initialState);
     const [err, setErr] = useState("");
+    const [hideForm, setHideForm] = useState(false);
 
     const onChange = (e) => {
         const {name, value} = e.target;
@@ -31,7 +34,10 @@ const LoginPage = () => {
                 localStorage.setItem("username", res.data.username);
                 localStorage.setItem("token", res.data.token);
 
-                window.location.replace("/");
+                setHideForm(true)
+
+                setTimeout(() => window.location.replace("/"), 800);
+
             })
             .catch(() => {
                 setErr("Wrong email or password");
@@ -49,31 +55,38 @@ const LoginPage = () => {
                 <div className="w-11/12 sm:w-80 font-dmsansRegular">
                     <div className="flex flex-col items-center">
                         <img className="w-20" src={Logo} alt="logo_img"/>
-                        <p className="welcomePageTitle text-xl">Login</p>
 
-                        <p className="mt-4 text-red">{err}</p>
+                        <div className={hideForm ? `block` : 'hidden'}>
+                            <Lottie animationData={CheckAnimation} />
+                        </div>
 
-                        <form className="w-full mt-5" onSubmit={onSubmit}>
-                            <AuthInput
-                                placeholder="Email..."
-                                name="email"
-                                type="text"
-                                onChange={onChange}
-                            />
-                            <AuthInput
-                                placeholder="Password..."
-                                name="password"
-                                type="password"
-                                onChange={onChange}
-                            />
-                            <AuthButton value="Submit"/>
-                        </form>
+                        <div className={hideForm ? `hidden` : 'block w-full'}>
+                            <p className="welcomePageTitle text-xl">Login</p>
 
-                        <NavLink to="/">
-                            <p className="transition hover:text-purple">
-                                go back
-                            </p>
-                        </NavLink>
+                            <p className="mt-4 text-red">{err}</p>
+
+                            <form className="w-full mt-5" onSubmit={onSubmit}>
+                                <AuthInput
+                                    placeholder="Email..."
+                                    name="email"
+                                    type="text"
+                                    onChange={onChange}
+                                />
+                                <AuthInput
+                                    placeholder="Password..."
+                                    name="password"
+                                    type="password"
+                                    onChange={onChange}
+                                />
+                                <AuthButton value="Submit"/>
+                            </form>
+
+                            <NavLink to="/">
+                                <p className="text-center transition hover:text-purple">
+                                    go back
+                                </p>
+                            </NavLink>
+                        </div>
                     </div>
                 </div>
             </div>
